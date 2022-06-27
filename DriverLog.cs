300ca9ea598da810cc2f92dd30e2d730561419ae
Jennifer Edelstein <jedelstein@ati-america.com>
2022-06-27 15:01:05 -0600

    refactor to reduce nesting

--- DriverLog.cs
+++ DriverLog.cs
@@ -11,22 +11,22 @@ namespace BlurayPlayer_Kaleidescape_IP
         {
             var logMessage = $"{logLevel}: {methodName} - {message}";
 
-            if (logEnabled)
+            if (!logEnabled) return;
+            switch (logLevel)
             {
-                switch (logLevel)
-                {
-                    case LoggingLevel.Error:
-                        ErrorLog.Error(logMessage);
-                        break;
+                case LoggingLevel.Error:
+                    ErrorLog.Error(logMessage);
+                    break;
 
-                    case LoggingLevel.Warning:
-                        ErrorLog.Warn(logMessage);
-                        break;
+                case LoggingLevel.Warning:
+                    ErrorLog.Warn(logMessage);
+                    break;
 
-                    case LoggingLevel.Debug:
-                        ErrorLog.Notice(logMessage);
-                        break;
-                }
+                case LoggingLevel.Debug:
+                    ErrorLog.Notice(logMessage);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
             }
 
         }
